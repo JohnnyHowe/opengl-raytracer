1c9a06ab6a4a9788214efd2a4c6c360b55c59d11
Johnny Howe <jonathon.o.howe@gmail.com>
2021-05-19 11:43:09 +1200

    Box

--- OpenGLProject/OpenGLProject/RayTracer.cpp
+++ OpenGLProject/OpenGLProject/RayTracer.cpp
@@ -29,6 +29,48 @@ void display() {
 	window.draw(scene);
 }
 
+void createBox(glm::vec3 center, glm::vec3 size, glm::vec3 color, float transparency=0.0f) {
+	// Yes this is super ugly but I have zero motivation right now
+	float minx = center.x - size.x / 2;
+	float maxx = center.x + size.x / 2;
+	float miny = center.y - size.y / 2;
+	float maxy = center.y + size.y / 2;
+	float minz = center.z - size.z / 2;
+	float maxz = center.z + size.z / 2;
+	Plane* plane1 = new Plane(glm::vec3(minx, miny, minz), glm::vec3(maxx, maxy, minz), glm::vec3(maxx, miny, minz), glm::vec3(minx, maxy, minz));
+	Plane* plane2 = new Plane(glm::vec3(minx, miny, maxz), glm::vec3(maxx, miny, maxz), glm::vec3(maxx, maxy, maxz), glm::vec3(minx, maxy, maxz));
+	Plane* plane3 = new Plane(glm::vec3(minx, maxy, minz), glm::vec3(minx, maxy, maxz), glm::vec3(maxx, maxy, maxz), glm::vec3(maxx, maxy, minz));
+	Plane* plane4 = new Plane(glm::vec3(minx, miny, minz), glm::vec3(minx, miny, maxz), glm::vec3(maxx, miny, maxz), glm::vec3(maxx, miny, minz));
+	Plane* plane5 = new Plane(glm::vec3(minx, miny, minz), glm::vec3(minx, maxy, minz), glm::vec3(minx, maxy, maxz), glm::vec3(minx, miny, maxz));
+	Plane* plane6 = new Plane(glm::vec3(maxx, miny, minz), glm::vec3(maxx, maxy, minz), glm::vec3(maxx, maxy, maxz), glm::vec3(maxx, miny, maxz));
+	scene.objects.push_back(plane1);
+	scene.objects.push_back(plane2);
+	scene.objects.push_back(plane3);
+	scene.objects.push_back(plane4);
+	scene.objects.push_back(plane5);
+	scene.objects.push_back(plane6);
+	plane1->setSpecularity(false);
+	plane2->setSpecularity(false);
+	plane3->setSpecularity(false);
+	plane4->setSpecularity(false);
+	plane5->setSpecularity(false);
+	plane6->setSpecularity(false);
+	plane1->setColor(color);
+	plane2->setColor(color);
+	plane3->setColor(color);
+	plane4->setColor(color);
+	plane5->setColor(color);
+	plane6->setColor(color);
+	if (transparency > 0) {
+		plane1->setTransparency(true, transparency);
+		plane2->setTransparency(true, transparency);
+		plane3->setTransparency(true, transparency);
+		plane4->setTransparency(true, transparency);
+		plane5->setTransparency(true, transparency);
+		plane6->setTransparency(true, transparency);
+	}
+}
+
 //---This function initializes the scene ------------------------------------------- 
 //   Specifically, it creates scene objects (spheres, planes, cones, cylinders etc)
 //     and add them to the list of scene objects.
@@ -51,25 +93,22 @@ void initialize()
 	scene.objects.push_back(plane);
 	plane->setSpecularity(false);
 
+	createBox(glm::vec3(10.0, -10.0, -70), glm::vec3(15, 5, 15), glm::vec3(1.0, 0.5, 0.5), 0.2f);
+
 	Sphere* sphere1 = new Sphere(glm::vec3(-5.0, 0.0, -90.0), 15.0);
 	//sphere1->setRefractivity(true, 0.9, 2);
 	sphere1->setColor(glm::vec3(0.5, 0.5, 1.0));   //Set colour to blue
 	scene.objects.push_back(sphere1);
 	//sphere1->setReflectivity(true, 1);
-	sphere1->setTransparency(true, 0.6f);
+	sphere1->setTransparency(true, 0.5f);
 
-	Sphere* sphere2 = new Sphere(glm::vec3(10, 3, -70.0), 5.0);
-	sphere2->setColor(glm::vec3(0, 1, 0));   //Set colour to blue
-	scene.objects.push_back(sphere2);
-	sphere2->setTransparency(true, 0.6f);
-
-	Sphere* sphere3 = new Sphere(glm::vec3(10.0, -10, -65.0), 3.0);
-	sphere3->setColor(glm::vec3(0, 0, 0));   //Set colour to blue
+	Sphere* sphere3 = new Sphere(glm::vec3(10.0, -4.0, -70.0), 3.0);
+	sphere3->setColor(glm::vec3(0, 1, 0));
 	scene.objects.push_back(sphere3);
 
-	//Sphere* sphere4 = new Sphere(glm::vec3(0.0, -15.0, -130.0), 20.0);
-	//sphere4->setColor(glm::vec3(0, 0, 1));
-	//scene.objects.push_back(sphere4);
+	Sphere* sphere4 = new Sphere(glm::vec3(0.0, -15.0, -150.0), 20.0);
+	sphere4->setColor(glm::vec3(0, 0, 1));
+	scene.objects.push_back(sphere4);
 
 }
 
